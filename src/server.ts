b8600db4397ac392f701ad107b382ed96a6b32ce
Bernhard Häussermann <bernhardgh@icloud.com>
2022-07-30 15:56:34 +0200

    Update express-openapi-validator to version 4 for compatibility with Node.js 16

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import * as bodyParser from 'body-parser';
 import * as http from 'http';
 import * as swaggerUi from 'swagger-ui-express';
 import * as yaml from 'yamljs';
-import { OpenApiValidator } from 'express-openapi-validator';
+import * as OpenApiValidator from 'express-openapi-validator';
 import { EmployeesRoutesRegistrar } from './routes/employees-routes-registrar';
 import { EmployeesController } from './controllers/employees-controller';
 
@@ -17,23 +17,22 @@ app.use(bodyParser.json());
 const apiSpec = yaml.load('./dist/api-spec.yaml');
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(apiSpec));
 
-new OpenApiValidator({
+const validator = OpenApiValidator.middleware({
   apiSpec: './dist/api-spec.yaml',
   validateRequests: true,
   validateResponses: true
-})
-.install(app)
-.then(() => {
-  EmployeesRoutesRegistrar.registerRoutes(app, new EmployeesController());
-
-  app.use((err, _req, res, _next) => {
-    console.error(err);
-    res.status(err.status || 500).json({
-      message: err.message,
-      errors: err.errors
-    });
-  });
-
-  http.createServer(app).listen(port);
-  console.log('Employees API server started on port ' + port);
 });
+app.use(validator);
+
+EmployeesRoutesRegistrar.registerRoutes(app, new EmployeesController());
+
+app.use((err, _req, res, _next) => {
+  console.error(err);
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors
+  });
+});
+
+http.createServer(app).listen(port);
+console.log('Employees API server started on port ' + port);
